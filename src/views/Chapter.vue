38758c4a321ff9b34c8e237b67fc1901269f7fef
Paweł Grabowski <pawel.grabowski5@gmail.com>
2020-05-31 13:48:39 +0200

    Typography improvements

--- src/views/Chapter.vue
+++ src/views/Chapter.vue
@@ -3,15 +3,16 @@
 		<md-app>
 			<md-app-content>
 				<div :key="id" v-for="{id, q, a, subchapter, title, content} in getChapter(id)">
-					<h2 v-if="subchapter">{{ title }}</h2>
-					<h3 v-if="!subchapter" :id="id">{{id}}. {{q}}</h3>
+					<h2 class="md-headline" v-if="subchapter">{{ title }}</h2>
+					<h4 class="md-subheading md-accent" v-if="!subchapter" :id="id">{{id}}. {{q}}</h4>
 					<div v-if="subchapter">
-						<div :key="id" v-for="{id, q, a } in content">
-							<h3 v-if="id" :id="id">{{ id }}. {{ q }}</h3>
-							<p v-html="a"></p>
+						<div :key="id" v-for="{id, q, a, subheader } in content">
+							<h3 class="md-title" v-if="subheader">{{subheader}}</h3>
+							<h4 class="md-subheading" v-if="id" :id="id">{{ id }}. {{ q }}</h4>
+							<p class="md-body-1" v-html="a"></p>
 						</div>
 					</div>
-					<p v-if="!subchapter" v-html="a"></p>
+					<p class="md-body-1" v-if="!subchapter" v-html="a"></p>
 				</div>
 			</md-app-content>
 		</md-app>
@@ -42,6 +43,9 @@
 	}
 	h2{
 		white-space: break-spaces;
+	}
+	h2, h3, h4{
+		margin: 0;
 	}
 	p{
 		white-space: pre-wrap;
