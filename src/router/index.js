6afe61a68e5592bad4e8b9176c1bb6cd0c1ab388
Paweł Grabowski <pawel.grabowski5@gmail.com>
2020-05-31 10:12:41 +0200

    index.js update

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Chapter from "../views/Chapter";
-import Home from '../views/Home.vue'
-
-Vue.use(VueRouter)
-
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/chapter/:id',
-    name: 'Chapter',
-    component: Chapter
-  }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
-
-export default router
-=======
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Chapter from '@/views/Chapter'
@@ -62,4 +33,3 @@ const router = new VueRouter({
 })
 
 export default router
->>>>>>> master
